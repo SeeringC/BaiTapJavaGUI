6bff90cc64c9f8955627b63ba00091d77b037a23
SeeringC <124576444+SeeringC@users.noreply.github.com>
2023-12-08 20:37:09 +0700

    3rd commit

--- src/main/java/com/example/delete/Facebook.java
+++ src/main/java/com/example/delete/Facebook.java
@@ -2,11 +2,12 @@ package com.example.delete;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 
-public class MessengerSelectionTab implements UILayer {
+public class Facebook implements UILayer {
     @FXML
     private Pane currentPane;
     @Override
--- src/main/java/com/example/delete/UIManager.java
+++ src/main/java/com/example/delete/UIManager.java
@@ -9,7 +9,7 @@ import java.util.Objects;
 public class UIManager extends Singleton<UIManager> {
 
     public <T extends UILayer> void openScene(Pane currentPane, String url) throws IOException {
-        FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(HelloController.class.getResource(url)));
+        FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(Facebook.class.getResource(url)));
         Pane nextPane = loader.load();
         T controller = loader.getController();
         controller.onInit();
